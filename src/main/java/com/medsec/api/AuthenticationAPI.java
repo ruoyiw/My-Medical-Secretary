f3e8f32885c98c62bb4dd5bd5057cc24a2645e38
Wenzhuo Mi <miw@student.unimelb.edu.au>
2018-08-05 03:40:02 +1000

    authentication API: renew, revoke

--- src/main/java/com/medsec/api/AuthenticationAPI.java
+++ src/main/java/com/medsec/api/AuthenticationAPI.java
@@ -2,14 +2,19 @@ package com.medsec.api;
 
 import com.medsec.ArgumentException;
 import com.medsec.AuthenticationException;
+import com.medsec.filter.Secured;
 import com.medsec.entity.User;
 import com.medsec.util.Database;
 import com.medsec.util.DefaultRespondEntity;
 import com.medsec.util.Token;
+import com.medsec.util.UserRole;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+import java.time.Instant;
 
 @Path("/")
 public class AuthenticationAPI {
@@ -30,6 +35,7 @@ public class AuthenticationAPI {
 
             // Return the token on the response, omit some sensitive information
             User respondUser = user
+                    .token_valid_from(null)
                     .password(null)
                     .role(null);
 
@@ -48,6 +54,42 @@ public class AuthenticationAPI {
         }
     }
 
+    @POST
+    @Secured
+    @Path("renewToken")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response renewToken(@Context SecurityContext securityContext) {
+
+        // Get ingredient from interceptor (uid and role)
+        User user = (User)securityContext.getUserPrincipal();
+        String uid = user.getId();
+        UserRole role = user.getRole();
+
+        // generate new token
+        Token token = Token.createToken(uid, role);
+
+        // respond response entity
+        User response = new User()
+                .token(token.getToken())
+                .token_expire_date(token.getExp());
+        return Response.ok(response).build();
+    }
+
+    @POST
+    @Secured()
+    @Path("revokeAll")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response revokeAllToken(@Context SecurityContext securityContext) {
+
+        // Get user id from interceptor
+        String uid = securityContext.getUserPrincipal().getName();
+
+        // Reset token_valid_from
+        Database db = new Database();
+        db.updateTokenValidFromDate(uid, Instant.now());
+        return Response.ok(new DefaultRespondEntity("Success")).build();
+    }
+
     private User authenticate(User u) throws ArgumentException, AuthenticationException {
         if (u.getEmail() == null || u.getPassword() == null)
             throw new ArgumentException();
@@ -61,9 +103,6 @@ public class AuthenticationAPI {
     }
 
     private User issueToken(User u) {
-        Token token = Token.createToken(u);
-        Database db = new Database();
-        db.updateToken(token);
-        return u;
+        return Token.createTokenForUser(u);
     }
 }
