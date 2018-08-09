093ce0890662da34914f3bd1e388ad9fe4fe0895
Wenzhuo Mi <miw@student.unimelb.edu.au>
2018-08-09 23:14:43 +1000

    UserAPI: activate, changePassword

--- src/main/java/com/medsec/util/AuthenticationException.java
+++ src/main/java/com/medsec/util/AuthenticationException.java
@@ -5,6 +5,9 @@ public class AuthenticationException extends Exception {
     public static final String INVALID_TOKEN = "Invalid token";
     public static final String TOKEN_EXPIRED = "Token expired";
 
+    public static final String REGISTRATION_NOT_MATCH = "Registered information does not match.";
+    public static final String USER_ALREADY_ACTIVATED = "User is already activated.";
+
     public AuthenticationException() {
         super(BAD_CREDENTIALS);
     }
